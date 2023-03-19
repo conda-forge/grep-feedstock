5d644bac17948cac7dea5461de3caf093a1466ae
Caleb Zulawski <caleb.zulawski@gmail.com>
2023-03-18 20:54:45 -0400

    Fix CROSSCOMPILE_EMULATOR not set

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,7 @@ set -euxo pipefail
 
 make -j${CPU_COUNT}
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
   make check
 fi
 
